b1072c6e0887264335bd13fac6ce3dacd49cf059
Cassimolar <LuciferGabriel@Live.cn>
2022-09-04 01:29:36 +0800

    井上 梨名

--- extensions/sakamichi.lua
+++ extensions/sakamichi.lua
@@ -18728,7 +18728,7 @@ sakamichi_mi_yan = sgs.CreateTriggerSkill {
         local use = data:toCardUse()
         if event == sgs.CardUsed or sgs.CardResponded then
             local card
-            if event == sgs.PreCardUsed then
+            if event == sgs.CardUsed then
                 card = data:toCardUse().card
             else
                 if data:toCardResponse().m_isUse then
@@ -18967,6 +18967,167 @@ sgs.LoadTranslationTable {
     [":sakamichi_ding_dian"] = "当你成为通常锦囊牌的目标时，若目标不唯一，你可以摸一张牌，然后你可以将一张手牌交给其中一个目标。",
     ["sakamichi_zi_you"] = "自由",
     [":sakamichi_zi_you"] = "锁定技，你使用牌无次数和距离限制。你的回合内，当你使用牌时，若你本回合内已使用过此牌名的牌，你摸一张牌。",
+}
+
+-- 井上 梨名
+RiraInoue_Keyakizaka = sgs.General(Sakamichi, "RiraInoue_Keyakizaka", "Keyakizaka46", 4, false)
+SKMC.NiKiSei.RiraInoue_Keyakizaka = true
+SKMC.SeiMeiHanDan.RiraInoue_Keyakizaka = {
+	name = {4, 3, 11, 6},
+	ten_kaku = {7, "ji"},
+	jin_kaku = {14, "xiong"},
+	ji_kaku = {17, "ji"},
+	soto_kaku = {10, "xiong"},
+	sou_kaku = {24, "da_ji"},
+	GoGyouSanSai = {
+		ten_kaku = "jin",
+		jin_kaku = "huo",
+		ji_kaku = "jin",
+		san_sai = "xiong",
+	},
+}
+
+sakamichi_hua_she_view_as = sgs.CreateOneCardViewAsSkill {
+    name = "sakamichi_hua_she",
+    filter_pattern = ".|.|.|hand",
+    guhuo_type = "lsr",
+    view_as = function(self, card)
+        local cd = sgs.Self:getTag(self:objectName()):toCard()
+        sgs.Self:removeTag(self:objectName())
+        cd:addSubcard(card:getId())
+        cd:setSkillName(self:objectName())
+        return cd
+    end,
+    enabled_at_play = function(self, player)
+        return not player:hasFlag("hua_she_used")
+    end,
+    enabled_at_response = function(self, player, pattern)
+        return player:getPhase() == sgs.Player_Play and not player:hasFlag("hua_she_used")
+    end,
+    enabled_at_nullification = function(self, player)
+        return player:getPhase() == sgs.Player_Play and not player:hasFlag("hua_she_used")
+    end,
+}
+sakamichi_hua_she = sgs.CreateTriggerSkill {
+    name = "sakamichi_hua_she",
+    view_as_skill = sakamichi_hua_she_view_as,
+    events = {sgs.CardUsed, sgs.CardResponded},
+    on_trigger = function(self, event, player, data, room)
+        local card
+        if event == sgs.PreCardUsed then
+            card = data:toCardUse().card
+        else
+            if data:toCardResponse().m_isUse then
+                card = data:toCardResponse().m_card
+            end
+        end
+        if card:getSkillName() == self:objectName() then
+            room:setPlayerFlag(player, "hua_she_used")
+        end
+        return false
+    end,
+}
+RiraInoue_Keyakizaka:addSkill(sakamichi_hua_she)
+
+sakamichi_chong_fuCard = sgs.CreateSkillCard {
+    name = "sakamichi_chong_fuCard",
+    skill_name = "sakamichi_chong_fu",
+    target_fixed = true,
+    on_use = function(self, room, source, targets)
+        room:setPlayerMark(source, "chong_fu", 1)
+    end,
+}
+sakamichi_chong_fu_view_as = sgs.CreateZeroCardViewAsSkill {
+    name = "sakamichi_chong_fu",
+    view_as = function(self)
+        return sakamichi_chong_fuCard:clone()
+    end,
+    enabled_at_play = function(self, target)
+        return not target:hasUsed("#sakamichi_chong_fuCard")
+    end,
+}
+sakamichi_chong_fu = sgs.CreateTriggerSkill {
+    name = "sakamichi_chong_fu",
+    view_as_skill = sakamichi_chong_fu_view_as,
+    events = {sgs.CardUsed, sgs.CardFinished},
+    on_trigger = function(self, event, player, data, room)
+        local use = data:toCardUse()
+        if event == sgs.CardUsed then
+            if use.card and (use.card:isKindOf("BasicCard") or use.card:isNDTrick()) and player:getMark("chong_fu") ~= 0 then
+                room:setCardFlag(use.card, "chong_fu")
+                room:setPlayerMark(player, "chong_fu", 0)
+                if use.card:isKindOf("Collateral") then
+                    for _, p in sgs.qlist(use.to) do
+                        local d = sgs.QVariant()
+                        local victim = p:getTag("collateralVictim"):toPlayer()
+                        d:setValue(victim)
+                        p:setTag("chong_fu_Collateral", d)
+                    end
+                end
+            end
+            if use.card:isKindOf("Collateral") and use.card:hasFlag("chong_fu_2") then
+                for _, p in sgs.qlist(use.to) do
+                    local pl_list = sgs.SPlayerList()
+                    for _, pl in sgs.qlist(room:getOtherPlayers(p)) do
+                        local p_list = sgs.PlayerList()
+                        p_list:append(p)
+                        if use.card:targetFilter(p_list, pl, player) then
+                            pl_list:append(pl)
+                        end
+                    end
+                    if pl_list:isEmpty() then
+                        use.to:removeOne(p)
+                    else
+                        local victim = room:askForPlayerChosen(player, pl_list, self:objectName(),
+                                                               "@chong_fu_collateral:" .. p:objectName() .. "::" ..
+                                                                   use.card:objectName())
+                        local _data = sgs.QVariant()
+                        _data:setValue(victim)
+                        p:setTag("collateralVictim", _data)
+                    end
+                end
+            end
+        else
+            if use.card:hasFlag("chong_fu") then
+                room:setCardFlag(use.card, "-chong_fu")
+                local targets = sgs.SPlayerList()
+                for _, p in sgs.qlist(use.to) do
+                    if not room:isProhibited(player, p, use.card) then
+                        if use.card:targetFixed() then
+                            if not use.card:isKindOf("Peach") or p:isWounded() then
+                                targets:append(p)
+                            end
+                        else
+                            if use.card:targetFilter(sgs.PlayerList(), p, player) then
+                                targets:append(p)
+                            end
+                        end
+                    end
+                end
+                if not targets:isEmpty() then
+                    room:setCardFlag(use.card, "chong_fu_2")
+                    room:useCard(sgs.CardUseStruct(use.card, player, targets, true), true)
+                end
+            end
+        end
+        return false
+    end,
+}
+RiraInoue_Keyakizaka:addSkill(sakamichi_chong_fu)
+
+sgs.LoadTranslationTable {
+    ["RiraInoue_Keyakizaka"] = "井上 梨名",
+    ["&RiraInoue_Keyakizaka"] = "井上 梨名",
+    ["#RiraInoue_Keyakizaka"] = "敢言无惧",
+    ["~RiraInoue_Keyakizaka"] = "あなたのために頑張ります！",
+    ["designer:RiraInoue_Keyakizaka"] = "Cassimolar",
+    ["cv:RiraInoue_Keyakizaka"] = "井上 梨名",
+    ["illustrator:RiraInoue_Keyakizaka"] = "Cassimolar",
+    ["sakamichi_hua_she"] = "滑舌",
+    [":sakamichi_hua_she"] = "出牌阶段限一次，你可以将一张手牌当任意基本牌或通常锦囊牌使用或打出。",
+    ["sakamichi_chong_fu"] = "重复",
+    [":sakamichi_chong_fu"] = "出牌阶段限一次，你使用的下一张基本牌或通常锦囊牌在结算完成后额外结算一次。",
+    ["@chong_fu_collateral"] = "请为此【%arg】的目标%src选择一个使用【杀】的目标",
 }
 
 sgs.Sanguosha:addSkills(SKMC.SkillList)

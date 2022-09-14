e217966bd6e893ab443a0616f6829e9ad81b3ef9
Cassimolar <LuciferGabriel@Live.cn>
2022-09-14 16:04:04 +0800

    潮 紗理菜

--- extensions/sakamichi.lua
+++ extensions/sakamichi.lua
@@ -5737,7 +5737,7 @@ sgs.LoadTranslationTable {
     ["@wei_zhi_invoke"] = "你可以弃置场上的一张牌",
     ["sakamichi_yuan_c"] = "元C",
     [":sakamichi_yuan_c"] = "你死亡时可以选择一名其他乃木坂46势力角色或武将牌上有主公技的角色，若其武将牌上有主公技你令其获得之，然后令其选择并获得一个未上场或已阵亡角色的主公技。",
-    ["@yuan_c_invoke"] = "你可以选择一名“乃木坂46”势力角色或武将牌上有主公技的角色",
+    ["@yuan_c_invoke"] = "你可以选择一名乃木坂46势力角色或武将牌上有主公技的角色",
 }
 
 -- 大和 里菜
@@ -12187,7 +12187,7 @@ sgs.LoadTranslationTable {
     [":sakamichi_chi_ze"] = "主公技，限定技，结束阶段，你可以选择一名其他角色，令其翻面且直到其下个回合开始，受到来自乃木坂46势力角色造成的伤害+1。",
     ["@chi_ze"] = "斥责",
     ["@chi_ze_invoke"] = "你可以选择一名其他角色发动【%arg】",
-    ["_damage_start_start_clear"] = "“乃木坂46”伤害+1",
+    ["_damage_start_start_clear"] = "乃木坂46伤害+1",
     ["sakamichi_bai_ya"] = "白牙",
     [":sakamichi_bai_ya"] = "锁定技，你的黑色牌均视为无色。黑色牌对你造成的伤害-1。",
     ["#bai_ya_damage"] = "%to的【%arg】被触发，%card对%to造成的伤害-%arg2",
@@ -20474,6 +20474,110 @@ sgs.LoadTranslationTable {
     ["sakamichi_xin_lai"] = "信赖",
     [":sakamichi_xin_lai"] = "其他角色准备阶段，你可以翻面并将所有手牌交给其，若如此做，本回合内其令一名角色进入濒死时，你复原武将牌并摸X张牌（X为濒死角色手牌数）。",
     ["sakamichi_xin_lai:invoke"] = "是否发动【%arg】将所有手牌交给%src",
+}
+
+-- 潮 紗理菜
+SarinaUshio_HiraganaKeyakizaka = sgs.General(Sakamichi, "SarinaUshio_HiraganaKeyakizaka", "HiraganaKeyakizaka46", 3,
+                                             false)
+SKMC.IKiSei.SarinaUshio_HiraganaKeyakizaka = true
+SKMC.SeiMeiHanDan.SarinaUshio_HiraganaKeyakizaka = {
+    name = {15, 10, 11, 11},
+    ten_kaku = {15, "da_ji"},
+    jin_kaku = {25, "ji"},
+    ji_kaku = {32, "ji"},
+    soto_kaku = {37, "ji"},
+    sou_kaku = {47, "da_ji"},
+    GoGyouSanSai = {
+        ten_kaku = "tu",
+        jin_kaku = "tu",
+        ji_kaku = "mu",
+        san_sai = "ji_xiong_hun_he",
+    },
+}
+
+sakamichi_shou_maiCard = sgs.CreateSkillCard {
+    name = "sakamichi_shou_maiCard",
+    skill_name = "sakamichi_shou_mai",
+    will_throw = false,
+    filter = function(self, targets, to_select)
+        return #targets == 0 and to_select:getHandcardNum() < sgs.Self:getHandcardNum() and
+                   not sgs.Self:hasFlag("shou_mai_" .. to_select:objectName())
+    end,
+    on_effect = function(self, effect)
+        local room = effect.from:getRoom()
+        room:obtainCard(effect.to, self:getEffectiveId(), true)
+        local target = room:askForPlayerChosen(effect.from, room:getOtherPlayers(effect.to), self:getSkillName(),
+                                               "@shou_mai_choice:" .. effect.to:objectName())
+        if not room:askForUseSlashTo(effect.to, target,
+                                     "@shou_mai_slash:" .. target:objectName() .. ":" .. effect.from:objectName() .. ":" ..
+                                         SKMC.number_correction(effect.from, 1)) then
+            room:damage(sgs.DamageStruct(self:getSkillName(), effect.from, effect.to,
+                                         SKMC.number_correction(effect.from, 1)))
+        end
+    end,
+}
+sakamichi_shou_mai = sgs.CreateOneCardViewAsSkill {
+    name = "sakamichi_shou_mai",
+    filter_pattern = ".|.|.|hand",
+    view_as = function(self, card)
+        local cd = sakamichi_shou_maiCard:clone()
+        cd:addSubcard(card)
+        return cd
+    end,
+    enabled_at_play = function(self, player)
+        for _, p in sgs.qlist(player:getSiblings()) do
+            if not player:hasFlag("shou_mai_" .. p:objectName()) and p:getHandcardNum() < player:getHandcardNum() then
+                return true
+            end
+        end
+        return false
+    end,
+}
+SarinaUshio_HiraganaKeyakizaka:addSkill(sakamichi_shou_mai)
+
+sakamichi_ge_pai = sgs.CreateTriggerSkill {
+    name = "sakamichi_ge_pai",
+    events = {sgs.CardsMoveOneTime},
+    on_trigger = function(self, event, player, data, room)
+        local move = data:toMoveOneTime()
+        if move.from and move.to_place == sgs.Player_DiscardPile and
+            bit32.band(move.reason.m_reason, sgs.CardMoveReason_S_MASK_BASIC_REASON) ==
+            sgs.CardMoveReason_S_REASON_DISCARD then
+            for i = 0, move.card_ids:length() - 1, 1 do
+                local id = move.card_ids:at(i)
+                if room:getCardPlace(id) == sgs.Player_DiscardPile and player:hasSkill(self) then
+                    local card = room:askForCard(player, sgs.Sanguosha:getCard(id):getClassName() .. "|.|.|hand",
+                                                "@ge_pai_show:::" .. SKMC.true_name(sgs.Sanguosha:getCard(id)), data,
+                                                sgs.Card_MethodNone)
+                    if card then
+                        room:showCard(player, card:getEffectiveId())
+                        room:obtainCard(player, id)
+                    end
+                end
+            end
+        end
+    end,
+    can_trigger = function(self, target)
+        return target
+    end,
+}
+SarinaUshio_HiraganaKeyakizaka:addSkill(sakamichi_ge_pai)
+
+sgs.LoadTranslationTable {
+    ["SarinaUshio_HiraganaKeyakizaka"] = "潮 紗理菜",
+    ["&SarinaUshio_HiraganaKeyakizaka"] = "潮 紗理菜",
+    ["#SarinaUshio_HiraganaKeyakizaka"] = "平假圣母",
+    ["~SarinaUshio_HiraganaKeyakizaka"] = "下からきました",
+    ["designer:SarinaUshio_HiraganaKeyakizaka"] = "Cassimolar",
+    ["cv:SarinaUshio_HiraganaKeyakizaka"] = "潮 紗理菜",
+    ["illustrator:SarinaUshio_HiraganaKeyakizaka"] = "Cassimolar",
+    ["sakamichi_shou_mai"] = "收买",
+    [":sakamichi_shou_mai"] = "出牌阶段每名角色限一次，你可以将一张手牌交给一名手牌少于你的角色，并令其对一名其他角色使用一张【杀】，若其未如此做，你对其造成1点伤害。",
+    ["@shou_mai_choice"] = "请选择%src使用【杀】的目标",
+    ["@shou_mai_slash"] = "请对%src使用一张【杀】否则受到来自%dest的%arg点伤害",
+    ["sakamichi_ge_pai"] = "歌牌",
+    [":sakamichi_ge_pai"] = "其他角色弃置牌时，你可以展示一张同名手牌，若如此做，你获得此牌。",
+    ["@ge_pai_show"] = "你可以展示一张【%arg】获得此【%arg】",
 }
 
 -- ====================================================================================================櫻坂46====================================================================================================--
